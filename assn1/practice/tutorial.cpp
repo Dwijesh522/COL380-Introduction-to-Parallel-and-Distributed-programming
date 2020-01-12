105503f2fd2c1915d5ef464250ec86399c426150
dwijesh522 <iddwijesh@gmail.com>
2020-01-12 16:53:56 +0530

    openMP getting started done

--- assn1/practice/tutorial.cpp
+++ assn1/practice/tutorial.cpp
@@ -174,11 +174,200 @@ void reduction(int vec_size, int id)
 		int max_element=-1;
 		#pragma omp parallel num_threads(NUM_THREADS)
 		{
-			#pragma omp for reduction(max: max_element)
+			#pragma omp for schedule(static, NUM_THREADS) reduction(max: max_element)
 			for(int i=0; i<vec_size; i++)	
 				max_element = max_element > vec[i] ? max_element : vec[i];
 		}
 		cout << "max_element: " << max_element << endl;
+	}
+}
+
+// printing a two dimensional vector
+void print_matrix(vector<vector<int>> mat, int r, int c)
+{
+	for(int i=0; i<r; i++)
+	{
+		for(int j=0; j<c; j++)	cout << mat[i][j] << " ";
+		cout << endl;
+	}
+}
+
+// collapse
+void collapse(int vec_size, int id)
+{	
+	// sequential execution
+	if(id == 0)
+	{
+		// initialization
+		vector<vector<int>> a, b, c;
+		for(int i=0; i<vec_size; i++)
+		{
+			vector<int> temp_vec;
+			for(int j=0; j<vec_size; j++) temp_vec.push_back(i);
+			a.push_back(temp_vec);
+			b.push_back(temp_vec);
+			c.push_back(temp_vec);
+		}
+		// modifing matrix a
+		for(int i=0; i<vec_size; i++)
+			for(int j=0; j<vec_size; j++)
+				a[i][j] = a[i][j] - b[i][j] - c[i][j];
+	}
+	else
+	{
+		// initialization
+		vector<vector<int>> a(vec_size, vector<int>(vec_size));
+		vector<vector<int>> b(vec_size, vector<int>(vec_size));
+		vector<vector<int>> c(vec_size, vector<int>(vec_size));
+		#pragma omp parallel for collapse(2) schedule(static, NUM_THREADS)
+		for(int i=0; i<vec_size; i++)
+			for(int j=0; j< vec_size; j++)
+			{
+				a[i][j] = i;
+				b[i][j] = i;
+				c[i][j] = i;
+			}
+		// modifing matrix a
+		#pragma omp parallel for collapse(2) schedule(static, NUM_THREADS)
+		for(int i=0; i<vec_size; i++)
+			for(int j=0; j<vec_size; j++)
+				a[i][j] = a[i][j] - b[i][j] - c[i][j];
+	}
+}
+
+// ordered
+void ordered_clause(int times, int id)
+{
+	// parallel code with and without ordering
+	if(id)
+	{
+		#pragma omp parallel for ordered num_threads(NUM_THREADS)
+		for(int i=0; i<times; i++)
+		{
+			cout << "unordered: " << i << endl;
+			#pragma omp ordered
+			cout << "ordered: " << i << endl;
+		}
+	}
+}
+
+// no wait
+void no_wait(int vec_size, int id)
+{
+	// seq code
+	if(id == 0)
+	{
+		int a;
+		for(int i=0; i<vec_size; i++)	a = i%1000;
+		for(int i=0; i<vec_size; i++)	a = i%1000;
+		for(int i=0; i<vec_size; i++)	a = i%1000;
+		for(int i=0; i<vec_size; i++)	a = i%1000;
+	}
+	// par code, default barriers
+	else if(id == 1)
+	{
+		int a;
+		#pragma omp parallel num_threads(NUM_THREADS)
+		{
+			#pragma omp for
+			for(int i=0; i<vec_size; i++)	a = (i)%1000;	
+			#pragma omp for
+			for(int i=0; i<vec_size; i++)	a = (i)%1000;
+			#pragma omp for
+			for(int i=0; i<vec_size; i++)	a = (i)%1000;
+			#pragma omp for
+			for(int i=0; i<vec_size; i++)	a = i%1000;
+		}
+	}
+	// par code, with no wait clause
+	else
+	{
+		int a;
+		#pragma omp parallel num_threads(NUM_THREADS)
+		{
+			#pragma omp for schedule(static, NUM_THREADS) nowait
+			for(int i=0; i<vec_size; i++)	a = i%1000;
+			#pragma omp for schedule(static, NUM_THREADS) nowait
+			for(int i=0; i<vec_size; i++)	a = i%1000;
+			#pragma omp for schedule(static, NUM_THREADS) nowait
+			for(int i=0; i<vec_size; i++)	a = i%1000;
+			#pragma omp for schedule(static, NUM_THREADS)
+			for(int i=0; i<vec_size; i++)	a = i%1000;
+		}
+	}
+}
+
+// sections
+void sections_clause(int vec_size, int id)
+{
+	vector<int> vec1(vec_size), vec2(vec_size), vec3(vec_size), vec4(vec_size);
+	// par code, sharing across for loops
+	if(id == 0)
+	{
+		#pragma omp parallel num_threads(NUM_THREADS)
+		{
+			#pragma omp sections
+			{
+				#pragma omp section
+				{
+					for(int i=0; i<vec_size; i++)	vec1[i] = i%1000 + (i/10)%1000;
+					for(int i=0; i<vec_size; i++)	vec1[i] = i%1000 + (i/10)%1000;
+					for(int i=0; i<vec_size; i++)	vec1[i] = i%1000 + (i/10)%1000;
+				}
+				#pragma omp section
+				{
+					for(int i=0; i<vec_size; i++)	vec2[i] = i%1000 + (i/10)%1000;
+					for(int i=0; i<vec_size; i++)	vec2[i] = i%1000 + (i/10)%1000;
+					for(int i=0; i<vec_size; i++)	vec2[i] = i%1000 + (i/10)%1000;
+				}
+				#pragma omp section
+				{
+					for(int i=0; i<vec_size; i++)	vec3[i] = i%1000 + (i/10)%1000;
+					for(int i=0; i<vec_size; i++)	vec3[i] = i%1000 + (i/10)%1000;
+					for(int i=0; i<vec_size; i++)	vec3[i] = i%1000 + (i/10)%1000;
+				}
+				#pragma omp section
+				{
+					for(int i=0; i<vec_size; i++)	vec4[i] = i%1000 + (i/10)%1000;
+					for(int i=0; i<vec_size; i++)	vec4[i] = i%1000 + (i/10)%1000;
+					for(int i=0; i<vec_size; i++)	vec4[i] = i%1000 + (i/10)%1000;
+				}
+			}
+		}
+	}
+	else
+	{
+		#pragma omp parallel num_threads(NUM_THREADS)
+		{
+			#pragma omp for schedule(static, NUM_THREADS)
+				for(int i=0; i<vec_size; i++)
+				{
+					vec1[i] = i%1000 + (i/10)%1000;
+					vec1[i] = i%1000 + (i/10)%1000;
+					vec1[i] = i%1000 + (i/10)%1000;
+				}
+			#pragma omp for schedule(static, NUM_THREADS)
+				for(int i=0; i<vec_size; i++)
+				{
+					vec2[i] = i%1000 + (i/10)%1000;
+					vec2[i] = i%1000 + (i/10)%1000;
+					vec2[i] = i%1000 + (i/10)%1000;
+				}
+			#pragma omp for schedule(static, NUM_THREADS)
+				for(int i=0; i<vec_size; i++)
+				{
+					vec3[i] = i%1000 + (i/10)%1000;
+					vec3[i] = i%1000 + (i/10)%1000;
+					vec3[i] = i%1000 + (i/10)%1000;
+				}
+			#pragma omp for schedule(static, NUM_THREADS)
+				for(int i=0; i<vec_size; i++)
+				{
+					vec4[i] = i%1000 + (i/10)%1000;
+					vec4[i] = i%1000 + (i/10)%1000;
+					vec4[i] = i%1000 + (i/10)%1000;
+				}
+		}
 	}
 }
 
@@ -189,26 +378,38 @@ int main()
 	// getting the current time
 	auto start = high_resolution_clock::now();
 	
-	reduction(vec_size, 0);
+	sections_clause(vec_size, 0);
 	
 	// getting the end time
 	auto end = high_resolution_clock::now();
 	// get the duration
 	auto duration = duration_cast<microseconds>(end - start);
-	cout << "seq time: " << duration.count() << endl;
+	cout << "case 1: " << duration.count() << endl;
 	cout << endl;
 //	//-------------------------------------------------------------------------------------------------------------------------
 	// getting the current time
 	start = high_resolution_clock::now();
 	
-	reduction(vec_size, 1);
+	sections_clause(vec_size, 1);
 	
 	// getting the end time
 	end = high_resolution_clock::now();
 	// get the duration
 	duration = duration_cast<microseconds>(end - start);
-	cout << "reduction time: " << duration.count() << endl;
+	cout << "case 2: " << duration.count() << endl;
 	cout << endl;
+//	//-------------------------------------------------------------------------------------------------------------------------
+	// getting the current time
+//	start = high_resolution_clock::now();
+//	
+//	no_wait(vec_size, 2);
+//	
+//	// getting the end time
+//	end = high_resolution_clock::now();
+//	// get the duration
+//	duration = duration_cast<microseconds>(end - start);
+//	cout << "case 3: " << duration.count() << endl;
+//	cout << endl;
 //	//-------------------------------------------------------------------------------------------------------------------------
 //	// getting the current time
 //	auto start = high_resolution_clock::now();
