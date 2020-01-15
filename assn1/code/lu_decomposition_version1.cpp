c54b016052f951c32e8f308cf5351c0c9ac6d497
dwijesh522 <iddwijesh@gmail.com>
2020-01-15 22:31:16 +0530

    optimizing further

--- assn1/code/lu_decomposition_version1.cpp
+++ assn1/code/lu_decomposition_version1.cpp
@@ -12,10 +12,10 @@ int NUM_THREADS = 4;
 int MATRIX_SIZE = 4;
 
 // Helper fuctions
-void print_matrix(vector<vector<float>* > mat, int r, int c);
+void print_matrix(vector<vector<double>* > mat, int r, int c);
 void print_vector(vector<int> permutation, int size);
-pair<int, float> get_maximum_element_index(vector<vector<float>* >& mat, int c, int r1, int r2);
-void print_error(vector<vector<float>*> &mat_dup, vector<int> &permutation, vector<vector<float>*> &lower, vector<vector<float>*> &upper);
+pair<int, double> get_maximum_element_index(vector<vector<double>* >& mat, int c, int r1, int r2);
+void print_error(vector<vector<double>*> &mat_dup, vector<int> &permutation, vector<vector<double>*> &lower, vector<vector<double>*> &upper);
 
 int main(int argc, char *argv[])
 {
@@ -33,18 +33,18 @@ int main(int argc, char *argv[])
 	sscanf(argv[2], "%d", &NUM_THREADS);
 
 	// below format will help us parallelize initialization of matrices.
-	// declaring matrices as two dimensional vector of pointer to floats
-	//vector<vector<float>* > mat(MATRIX_SIZE, new vector<float>(MATRIX_SIZE, 0));
-	vector<vector<float>* > mat, upper, lower, mat_dup;
+	// declaring matrices as two dimensional vector of pointer to doubles
+	//vector<vector<double>* > mat(MATRIX_SIZE, new vector<double>(MATRIX_SIZE, 0));
+	vector<vector<double>* > mat, upper, lower, mat_dup;
 	// declaring permutation matrix as vector of pointers to integers
 	vector<int> permutation(MATRIX_SIZE);
 
 	// seed the random number generator
-	srand48(time(0));
+	srand48(time(0));											/// seed differently for each thread ###################
 
 	// ------------------------------------------------------------------------------------------
-	// ------------- sequential timing	: 3.5 sec (two dimensional matrix pointing to floats)
-	// ------------- parallel timing	: 2.9 sec (two dimensional matrix pointing to floats)
+	// ------------- sequential timing	: 3.5 sec (two dimensional matrix pointing to doubles)
+	// ------------- parallel timing	: 2.9 sec (two dimensional matrix pointing to doubles)
 	// ------------------------------------------------------------------------------------------
 	// intializing all four matrices
 	#pragma omp parallel num_threads(NUM_THREADS)
@@ -55,21 +55,21 @@ int main(int argc, char *argv[])
 			{
 				for(int i=0; i<MATRIX_SIZE; i++)
 				{
-					mat.push_back(new vector<float>(MATRIX_SIZE, float(0)));
-					mat_dup.push_back(new vector<float>(MATRIX_SIZE, float(0)));
+					mat.push_back(new vector<double>(MATRIX_SIZE, (0)));
+					mat_dup.push_back(new vector<double>(MATRIX_SIZE, (0)));
 				}
 			}
 			#pragma omp section
 			{
 				for(int i=0; i<MATRIX_SIZE; i++)
-					upper.push_back(new vector<float>(MATRIX_SIZE, float(0)));
+					upper.push_back(new vector<double>(MATRIX_SIZE, (0)));
 			}
 			#pragma omp section
 			{
 				for(int i=0; i<MATRIX_SIZE; i++)
 				{
-					lower.push_back(new vector<float>(MATRIX_SIZE, float(0)));
-					(*lower[i])[i] = float(1);
+					lower.push_back(new vector<double>(MATRIX_SIZE, (0)));
+					(*lower[i])[i] = (1);
 				}
 			}
 			#pragma omp section
@@ -78,37 +78,37 @@ int main(int argc, char *argv[])
 					permutation[i] = i;
 			}
 		}
-		// Initializing mat: 64000000 elements with random floating point values
+		// Initializing mat: 64000000 elements with random doubleing point values
 		#pragma omp for schedule(static, 4) collapse(2) nowait
 		for(int i=0; i<MATRIX_SIZE; i++)
 		{
 			for(int j=0; j<MATRIX_SIZE; j++)
 			{
-				float random_num = float(drand48());
+				double random_num = (drand48());
 				(*mat[i])[j] = random_num;
 				(*mat_dup[i])[j] = random_num;
 			}
 		}
 		// --------------------- only for debugging ----------------------------
-		(*mat[0])[0] = 1; (*mat_dup[0])[0] = 1;
-		(*mat[0])[1] = 2; (*mat_dup[0])[1] = 2;
-		(*mat[0])[2] = 3; (*mat_dup[0])[2] = 3;
-		(*mat[1])[0] = 3; (*mat_dup[1])[0] = 3;
-		(*mat[1])[1] = 1; (*mat_dup[1])[1] = 1;
-		(*mat[1])[2] = 2; (*mat_dup[1])[2] = 2;
-		(*mat[2])[0] = 2; (*mat_dup[2])[0] = 2;
-		(*mat[2])[1] = 3; (*mat_dup[2])[1] = 3;
-		(*mat[2])[2] = 1; (*mat_dup[2])[2] = 1;
+	//	(*mat[0])[0] = 1; (*mat_dup[0])[0] = 1;
+	//	(*mat[0])[1] = 2; (*mat_dup[0])[1] = 2;
+	//	(*mat[0])[2] = 3; (*mat_dup[0])[2] = 3;
+	//	(*mat[1])[0] = 3; (*mat_dup[1])[0] = 3;
+	//	(*mat[1])[1] = 1; (*mat_dup[1])[1] = 1;
+	//	(*mat[1])[2] = 2; (*mat_dup[1])[2] = 2;
+	//	(*mat[2])[0] = 2; (*mat_dup[2])[0] = 2;
+	//	(*mat[2])[1] = 3; (*mat_dup[2])[1] = 3;
+	//	(*mat[2])[2] = 1; (*mat_dup[2])[2] = 1;
 		// ---------------------------------------------------------------------
 	}
 
 	// outermost loop: non parallelizable due to data dependecies across iterations
 	for(int k=0; k< MATRIX_SIZE; k++)
 	{
-		pair<int, float> max_of_column;
+		pair<int, double> max_of_column;
 		max_of_column = get_maximum_element_index(mat, k, k, MATRIX_SIZE-1);
 		int k_dash = max_of_column.first;
-		float max_element = max_of_column.second;
+		double max_element = max_of_column.second;
 		// if singular matrix then stop
 		if(k_dash == -1)	{ cout << "Singular matrix.\n"; return 0;}
 		
@@ -116,7 +116,7 @@ int main(int argc, char *argv[])
 		// This operation can not be parallelized because second last for loop assumes
 		// two rows are swapped. If we do it parallely then second last for loop may
 		// read non-swapped elements.
-		vector<float>* temp_row_pointer = mat[k];
+		vector<double>* temp_row_pointer = mat[k];
 		mat[k] = mat[k_dash];
 		mat[k_dash] = temp_row_pointer;
 		// coping first (k-1) elements from kth row of lower triangular matrix because
@@ -124,9 +124,15 @@ int main(int argc, char *argv[])
 		// pointers and copy the content into some temporary variable is time consuming
 		// instead copy the whole subvector content to a temp vector and use that in 
 		// swapping elements. This will reduce order(n^2) instruction.
-		auto start = (*lower[k]).cbegin();
-		auto end = (*lower[k]).cbegin()+k;
-		vector<float> temp_sub_vector_lower_matrix(start, end);
+		auto start = (*lower[k]).begin();
+		auto end = (*lower[k]).begin()+k;
+		vector<double> temp_sub_vector_lower_matrix(start, end);
+
+		start = (*lower[k_dash]).begin();
+		end = (*lower[k_dash]).begin() + k;
+		vector<double> temp_sub_vector_lower_matrix_kdash(start, end);
+
+		vector<double> temp_sub_vector_upper_kth_row;
 		// starting a parallel section
 		#pragma omp parallel num_threads(NUM_THREADS)
 		{
@@ -144,22 +150,30 @@ int main(int argc, char *argv[])
 			#pragma omp for schedule(static, 4) nowait
 			for(int j=0; j<=k-1; j++)
 			{
-				(*lower[k])[j] = (*lower[k_dash])[j];
+				(*lower[k])[j] = temp_sub_vector_lower_matrix_kdash[j];
 				(*lower[k_dash])[j] = temp_sub_vector_lower_matrix[j];
 			}
 			// updating fractions in lower triangular matrix
 			// and elements of upper triangular matrix
-			#pragma omp for schedule(static, 4) nowait
+			#pragma omp for schedule(static, 4)
 			for(int j=k+1; j<MATRIX_SIZE; j++)
 			{
 				(*lower[j])[k] = (*mat[j])[k]/max_element;
 				(*upper[k])[j] = (*mat[k])[j];
 			}
+			#pragma omp single
+			{
+				auto start = (*upper[k]).begin() + k+1;
+				auto end = (*upper[k]).end();
+			}
 			// finally updating the input matrix
-			#pragma omp for schedule(static, 4) collapse(2)
+			#pragma omp for schedule(static, 4)
 			for(int i=k+1; i<MATRIX_SIZE; i++)
+			{
+				double operand1 = (*lower[i])[k];
 				for(int j=k+1; j<MATRIX_SIZE; j++)
-					(*mat[i])[j] += (((*lower[i])[k] * (*upper[k])[j]) * (-1));
+					(*mat[i])[j] += ((operand1 * (*upper[k])[j]) * (-1));
+			}
 		}
 	}
 	// getting the end time
@@ -168,16 +182,17 @@ int main(int argc, char *argv[])
 	auto duration = duration_cast<microseconds>(end - start);
 	cout << "Time taken: " << duration.count() << " micro seconds" << endl;
 
-	print_matrix(lower, MATRIX_SIZE, MATRIX_SIZE);
-	print_matrix(upper, MATRIX_SIZE, MATRIX_SIZE);
-	print_vector(permutation, MATRIX_SIZE);
+//	print_matrix(lower, MATRIX_SIZE, MATRIX_SIZE);
+//	print_matrix(upper, MATRIX_SIZE, MATRIX_SIZE);
+//	print_vector(permutation, MATRIX_SIZE);
+//	print_matrix(mat, MATRIX_SIZE, MATRIX_SIZE);
 
 	return 0;
 }
 // This function takes original input, permutation matrix, lower and upper triangular matrix as input and calculates the error
 // First rearranges the original input matrix a/c to permutation matrix. Then multiplies lower and upper triangular matrix and 
 // subtracts the result with rearranged matrix. and prints the error.
-void print_error(vector<vector<float>*> &mat_dup, vector<int> &permutation, vector<vector<float>*> &lower, vector<vector<float>*> &upper)
+void print_error(vector<vector<double>*> &mat_dup, vector<int> &permutation, vector<vector<double>*> &lower, vector<vector<double>*> &upper)
 {
 
 }
@@ -187,13 +202,13 @@ void print_error(vector<vector<float>*> &mat_dup, vector<int> &permutation, vect
 // Else it returns (-1, 0).
 // While comparing absolute values is compared against each other
 // ------------------------------------------------------------------------------------------------------
-// ------------- sequential time:	4.9 seconds (two dimensional matrix pointing to floats)
-// ------------- parallel time:		4.9 seconds (two dinensional matrix pointing to floats)
-// ------------- parallel time: 	2.2 seconds (one dimensional vector pointing to vector of floats)
+// ------------- sequential time:	4.9 seconds (two dimensional matrix pointing to doubles)
+// ------------- parallel time:		4.9 seconds (two dinensional matrix pointing to doubles)
+// ------------- parallel time: 	2.2 seconds (one dimensional vector pointing to vector of doubles)
 // ------------------------------------------------------------------------------------------------------
-pair<int, float> get_maximum_element_index(vector<vector<float>* > &mat, int c, int r1, int r2)
+pair<int, double> get_maximum_element_index(vector<vector<double>* > &mat, int c, int r1, int r2)
 {
-	float max_element=0;
+	double max_element=0;
 	int index=-1;
 	for(int i= r1; i<= r2; i++)
 	{
@@ -206,7 +221,7 @@ pair<int, float> get_maximum_element_index(vector<vector<float>* > &mat, int c, 
 	return make_pair(index, max_element);
 }
 
-void print_matrix(vector<vector<float>* > mat, int r, int c)
+void print_matrix(vector<vector<double>* > mat, int r, int c)
 {
 	for(int i=0; i<r; i++)
 	{
